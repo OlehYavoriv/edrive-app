9882a76bdcb1e335914062b7f47fcc8f7aa9fca7
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-10-22 12:52:49 +0300

    added routing and base structure for pages

---
+++ client/src/pages/CreateTestPage/CreateTestPage.tsx
@@ -0,0 +1,7 @@
+export const CreateTestPage = () => {
+    return (
+        <div>
+            CreateTestPage
+        </div>
+    );
+};
--- client/src/routes/Routing.tsx
+++ client/src/routes/Routing.tsx
@@ -1,9 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { authRoutes, publicRoutes } from "./paths";
+import { HOME_ROUTE } from "../utils/consts";
 
 export const Routing = () => {
+    const isAuth = true
     return (
         <Routes>
-            <Route path='/' element={<p>Edrive</p>}/>
+            {isAuth && authRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component/>}/>
+            )}
+            {publicRoutes.map(({path, Component}) =>
+                <Route key={path} path={path} element={<Component/>}/>
+            )}
+            <Route path='*' element={<Navigate to={HOME_ROUTE}/>}/>
         </Routes>
     );
 };
